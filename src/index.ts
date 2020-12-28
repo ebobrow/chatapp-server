1c483af94d1f895102b61fdf095f223d63d7be69
Elliot Bobrow <elliotgreybobrow@gmail.com>
2020-12-27 16:14:46 -0800

    Basics for chat app

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,33 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import authRoutes from './routes/users';
+import { Server, Socket } from 'socket.io';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+});
 const PORT = process.env.PORT || 4000;
+
+io.on('connection', (socket: Socket) => {
+  console.log('connected');
+
+  socket.on('new-message', message => {
+    console.log(message);
+    socket.broadcast.emit('message', { message, sender: socket.id });
+  });
+});
 
 app.use(morgan('dev'));
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });

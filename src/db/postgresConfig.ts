cdf7cbc9507990a21e3b7320e980485d11cf7515
Elliot Bobrow <elliotgreybobrow@gmail.com>
2021-02-15 22:03:01 -0800

    Fix sql logging

--- src/db/postgresConfig.ts
+++ src/db/postgresConfig.ts
@@ -3,16 +3,14 @@ import chalk from 'chalk';
 
 const pool = new Pool();
 
+// Probably very slow but it's only for development
 const parseLog = (sql: string) =>
   sql
+    .split('')
+    .map(char => (/[A-Z]/.test(char) ? chalk.blue(char) : char))
+    .join('')
     .split(' ')
-    .map(word =>
-      word.startsWith('$') // Params are green, to match strings in array
-        ? chalk.green(word)
-        : word === word.toUpperCase() && word !== '=' // Assuming all sql is uppercase
-        ? chalk.blue(word)
-        : word
-    )
+    .map(word => (word.startsWith('$') ? chalk.green(word) : word))
     .join(' ');
 
 export const query = async (
